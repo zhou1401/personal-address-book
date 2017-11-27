fb59950f89848072afdcec7a75dcc9ca2b223551
jyz <jyz@jyz4iMac.local>
2017-11-27 20:10:13 +0800

    界面搭建

---
+++ day-13私人通讯录/day-13私人通讯录/ListTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ListTableViewController.swift
+//  day-13私人通讯录
+//
+//  Created by jyz on 2017/11/27.
+//  Copyright © 2017年 HDGS. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
