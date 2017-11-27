49c9d78d262bdbd1aa5ec4148779187bd4991bc7
jyz <jyz@jyz4iMac.local>
2017-11-27 20:13:59 +0800

    定义模型

---
+++ day-13私人通讯录/day-13私人通讯录/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  day-13私人通讯录
+//
+//  Created by jyz on 2017/11/27.
+//  Copyright © 2017年 HDGS. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name:String?
+    var phone:String?
+    var title:String?
+    
+}
